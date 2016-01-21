4aa0684209fb9b527e35e71496a9a5a6d4cba981
Kissa1001 <kissa101295@gmail.com>
2016-01-21 16:01:12 -0500

    files added

---
+++ js/app.js
@@ -0,0 +1,38 @@
+$(function(){
+  $('#search').submit(function(event){
+    event.preventDefault();
+    var searchValue = $('#query').val();
+  getRequest(searchValue);
+  });
+});
+
+  function getRequest(searchValue){
+    var params = {
+      part: 'snippet',
+      key: 'AIzaSyCb3EOFPB9g7VsCxkGxTaJGX74a3opBbJI',
+      q: searchValue
+    }
+    url='https://www.googleapis.com/youtube/v3/search';
+
+
+
+    var container = $('ul.results')
+
+    $.getJSON(url, params, function(data) {
+      var results = $.map(data.items,function(result){
+          return {
+              thumbnails : result.snippet.thumbnails.medium.url,
+              title : result.snippet.title,
+              videoId : result.id.videoId,
+              url: 'https://www.youtube.com/watch?v=' + result.id.videoId
+          };
+      });
+        var attachTemplate = function(){
+        var sourse = $('#youtube-template').html();
+        var template = Handlebars.compile(sourse);
+        container.append( template(results) );
+        };
+
+      attachTemplate();
+    })
+  };
